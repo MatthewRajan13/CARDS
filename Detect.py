54ad200472454e81cd357d396293429dbf1ef9d8
MatthewRajan13 <89418442+MatthewRajan13@users.noreply.github.com>
2023-11-24 10:46:15 -0500

    cleaned output

--- Detect.py
+++ Detect.py
@@ -1,6 +1,17 @@
 import cv2
 from ultralytics import YOLO
 
+
+def clean_output(cards):
+    cards = list(set(cards))
+    cards = [card[:-1] for card in cards]
+
+    face_map = {'J': 10, 'Q': 10, 'K': 10, 'A': 'A'}
+    cards = [face_map[card] if card in face_map else int(card) for card in cards]
+
+    return cards
+
+
 model = YOLO('C:/Users/matth/runs/detect/train5/weights/best.pt')
 model.to('cuda')
 
@@ -8,9 +19,15 @@ cap = cv2.VideoCapture(0)
 
 while cap.isOpened():
     success, frame = cap.read()
-
+    cards = []
     if success:
         results = model(frame)
+        for result in results:
+            for i in range(int(result.boxes.cls.shape[0])):
+                name = result.names[int(result.boxes.cls[i])]
+                cards.append(name)
+
+        print(clean_output(cards))
 
         annotated_frame = results[0].plot()
 
