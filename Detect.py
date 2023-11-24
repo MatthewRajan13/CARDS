21a6a077a0e3d2464e740898404d42d8221d2515
MatthewRajan13 <89418442+MatthewRajan13@users.noreply.github.com>
2023-11-24 09:54:01 -0500

    Trained model and wrote a detection script

---
+++ Detect.py
@@ -0,0 +1,25 @@
+import cv2
+from ultralytics import YOLO
+
+model = YOLO('C:/Users/matth/runs/detect/train5/weights/best.pt')
+model.to('cuda')
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    success, frame = cap.read()
+
+    if success:
+        results = model(frame)
+
+        annotated_frame = results[0].plot()
+
+        cv2.imshow('YOLOv8', annotated_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
